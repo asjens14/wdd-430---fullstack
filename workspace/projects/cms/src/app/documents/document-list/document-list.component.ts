31a80a802105906cb55922708819f0331b5f9621
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-02-02 22:38:03 -0700

    update document components
    update document list components
    update document detail components
    update document item components
    update document component
    add dropdown directive

--- workspace/projects/cms/src/app/documents/document-list/document-list.component.ts
+++ workspace/projects/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+    @Output() selectedDocumentEvent = new EventEmitter<Document>();
+    documents:Document[] = [
+        new Document(1, 'Document 1', 'test doc 1', 'url', 'child'),
+        new Document(1, 'Document 2', 'test doc 2', 'url', 'child'),
+        new Document(1, 'Document 3', 'test doc 3', 'url', 'child')
+    ];
 
+    onSelectedDocument(document:Document) {
+        this.selectedDocumentEvent.emit(document);
+    }
 }
