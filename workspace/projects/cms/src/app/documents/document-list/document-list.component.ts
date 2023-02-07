b344f3f576be1e9cf95d9d40a1f6d8fd47b26f03
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-02-06 19:59:28 -0700

    add implementation of document service

--- workspace/projects/cms/src/app/documents/document-list/document-list.component.ts
+++ workspace/projects/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,20 +1,21 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent {
-    @Output() selectedDocumentEvent = new EventEmitter<Document>();
-    documents:Document[] = [
-        new Document(1, 'Document 1', 'test doc 1', 'url', 'child'),
-        new Document(1, 'Document 2', 'test doc 2', 'url', 'child'),
-        new Document(1, 'Document 3', 'test doc 3', 'url', 'child')
-    ];
+export class DocumentListComponent implements OnInit {
+    documents:Document[] = [];
+    constructor(private documentService:DocumentService){}
 
+    ngOnInit() {
+        this.documents = this.documentService.getDocuments();
+    }
+    
     onSelectedDocument(document:Document) {
-        this.selectedDocumentEvent.emit(document);
+        this.documentService.documentSelectedEvent.emit(document);
     }
 }
