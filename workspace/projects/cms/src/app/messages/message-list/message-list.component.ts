92c37ff4e8c6cfcdcec049f26ece2ad2a26c5556
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-02-06 22:08:32 -0700

    Update message-list.component.ts

--- workspace/projects/cms/src/app/messages/message-list/message-list.component.ts
+++ workspace/projects/cms/src/app/messages/message-list/message-list.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Message } from '../message.model';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent {
+export class MessageListComponent implements OnInit{
     messages:Message[] = [
-        new Message(0, 'Test message', 'This is a test message', 'Jensen'),
-        new Message(1, 'Test message 2', 'This is a test message 2', 'Jensen'),
-        new Message(2, 'Test message 3', 'This is a test message 3', 'Jensen')
+        // new Message('0', 'Test message', 'This is a test message', 'Jensen'),
+        // new Message('1', 'Test message 2', 'This is a test message 2', 'Jensen'),
+        // new Message('2', 'Test message 3', 'This is a test message 3', 'Jensen')
     ];
+
+    messageSender:string;
+
+    constructor(private messageService:MessageService){ }
+
+    ngOnInit() {
+        this.messages = this.messageService.getMessages();
+    }
 
     onAddMessage(message:Message) {
         this.messages.push(message);
