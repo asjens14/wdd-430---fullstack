ee69d674b0cfa6fdf05f2b17c464c6b97da48fac
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-02-15 15:09:37 -0700

    view button working

--- workspace/projects/cms/src/app/documents/document-detail/document-detail.component.ts
+++ workspace/projects/cms/src/app/documents/document-detail/document-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params} from '@angular/router'
+import { WindRefService } from '../../wind-ref.service';
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
 
@@ -12,12 +13,16 @@ export class DocumentDetailComponent implements OnInit{
     // @Input() document:Document;
     document: Document;
     id: number;
+    nativeWindow: any;
 
     constructor(
         private documentService:DocumentService,
+        private windowRefService: WindRefService,
         private route: ActivatedRoute,
         private router: Router
-    ){}
+    ){
+        this.nativeWindow = windowRefService.getNativeWindow();
+    }
 
     ngOnInit(){
         this.route.params.subscribe((params: Params) =>{
@@ -25,5 +30,11 @@ export class DocumentDetailComponent implements OnInit{
             this.document = this.documentService.getDocument(this.id.toString());
             console.log(this.id);
         })
+    }
+
+    onView(){
+        if (this.document.url){
+            this.nativeWindow.open(this.document.url)
+        }
     }
 }
