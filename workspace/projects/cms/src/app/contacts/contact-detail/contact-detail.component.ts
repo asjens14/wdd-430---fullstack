be9a8da974e23ea2747a0aa13408801e629fdffe
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-02-15 17:23:37 -0700

    load contactDetailComponent working

--- workspace/projects/cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ workspace/projects/cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Contact} from '../contact.model';
+import { ContactService } from '../contact.service';
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.css']
 })
-export class ContactDetailComponent {
-    @Input() contact:Contact;
-    contacts: Contact[] = []
+export class ContactDetailComponent implements OnInit{
+    // @Input() contact:Contact;
+    // contacts: Contact[] = [];
+    id: number;
+    contact: Contact;
+
+    constructor(
+        private contactService:ContactService,
+        private route: ActivatedRoute,
+        private router: Router
+        ){}
+
+    ngOnInit() {
+        this.route.params.subscribe((params: Params) =>{
+            this.id = +params['id'];
+            this.contact = this.contactService.getContact(this.id.toString());
+            console.log(this.id);
+        })
+    }
 }
