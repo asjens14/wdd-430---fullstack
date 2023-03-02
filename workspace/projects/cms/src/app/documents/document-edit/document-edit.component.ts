f3ee69c859dba0bab7ebfeb17379cf8f4e5003cd
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-03-01 20:10:41 -0700

    Update document-edit.component.ts
    Add onSubmit function, add onInit function

--- workspace/projects/cms/src/app/documents/document-edit/document-edit.component.ts
+++ workspace/projects/cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrls: ['./document-edit.component.css']
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit{
+    originalDocument: Document;
+    document: Document;
+    editMode: boolean = false;
+    id:number;
 
+    constructor(
+        private documentService:DocumentService,
+        private router: Router,
+        private route: ActivatedRoute){}
+
+    ngOnInit(): void {
+        this.route.params.subscribe((params:Params) =>{
+            this.id = +params['id'];
+            if (!this.id || this.id === null){
+                this.editMode = false;
+                return;
+            }
+            this.originalDocument = this.documentService.getDocument(this.id.toString());
+            if (!this.originalDocument || this.originalDocument === null){
+                return;
+            }
+            this.editMode = true;
+            this.document = JSON.parse(JSON.stringify(this.originalDocument));
+        })
+    }
+
+    onSubmit(form:NgForm){
+        const value = form.value;
+        let newDocument = new Document(value.id,value.name, value.description, value.url, value.children);
+
+        if (this.editMode === true){
+            this.documentService.updateDocument(this.originalDocument,newDocument);
+        }
+        else {
+            this.documentService.addDocument(newDocument);
+        }
+        
+        this.router.navigate(['/documents'], {relativeTo: this.route})
+    }
 }
