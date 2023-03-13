35b2b6acbc3216d627938108e76b4449c1aad8bc
asjens14 <56127260+asjens14@users.noreply.github.com>
2023-03-13 16:39:55 -0600

    add message get functionality
    messages get functionality working
    messages put functionality not working

--- workspace/projects/cms/src/app/messages/message-item/message-item.component.ts
+++ workspace/projects/cms/src/app/messages/message-item/message-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Message } from '../message.model';
 import { ContactService } from '../../contacts/contact.service';
 import { Contact } from '../../contacts/contact.model';
@@ -8,15 +9,24 @@ import { Contact } from '../../contacts/contact.model';
     templateUrl: './message-item.component.html',
     styleUrls: ['./message-item.component.css']
 })
-export class MessageItemComponent implements OnInit {
+export class MessageItemComponent implements OnInit, OnDestroy {
     @Input() message: Message;
-
     messageSender: string;
+    subscription:Subscription;
 
     constructor(private contactService: ContactService) { }
 
     ngOnInit() {
-        const contact: Contact = this.contactService.getContact(this.message.sender);
-        this.messageSender = contact.name;
+        let contact: Contact = this.contactService.getContact(this.message.sender);
+        this.messageSender = contact?.name;
+        this.subscription = this.contactService.contactListChangedEvent
+        .subscribe(() => {
+            contact = this.contactService.getContact(this.message.sender);
+            this.messageSender = contact.name
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe()
     }
 }
--- workspace/projects/cms/src/app/messages/message.service.ts
+++ workspace/projects/cms/src/app/messages/message.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
 import { Message } from './message.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
 
 @Injectable({
@@ -8,14 +9,28 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
 export class MessageService {
     messages: Message[] = [];
     messageChangedEvent = new EventEmitter<Message[]>();
+    private maxMessageId:number;
 
-    constructor() {
-        this.messages = MOCKMESSAGES;
+    constructor(private http: HttpClient) {
+        // this.messages = MOCKMESSAGES;
+        this.maxMessageId = this.getMaxId()
     }
 
-    getMessages() {
+    getMessages(): Message[] {
+        this.http.get('https://asjcms-a34a0-default-rtdb.firebaseio.com/messages.json')
+          .subscribe(
+            (messages: Message[]) => {
+              this.messages = messages;
+              this.maxMessageId = this.getMaxId();
+              this.messages.sort((a, b)=> (a.id < b.id ? -1 : a.id > b.id ? 1 : 0));
+              this.messageChangedEvent.next(this.messages.slice());
+            }, 
+            (error: any) => {
+              console.log(error.message);
+            }
+        );
         return this.messages.slice();
-    }
+      }
 
     getMessage(id: string) {
         for (let message of this.messages) {
@@ -28,6 +43,32 @@ export class MessageService {
 
     addMessage(message:Message){
         this.messages.push(message);
-        this.messageChangedEvent.emit(this.messages.slice());
+        // this.messageChangedEvent.emit(this.messages.slice());
+        this.storeMessages();
     }
+
+    storeMessages() {
+        let json = JSON.stringify(this.messages);
+        this.http.put(
+          'https://cms-project-7478d-default-rtdb.firebaseio.com/messages.json', json, 
+          {
+            headers: new HttpHeaders({'Content-Type':'application/json'})
+          }
+        ). subscribe(() => {
+          this.messageChangedEvent.emit(this.messages.slice());
+        })
+      }
+
+    getMaxId(): number {
+        let maxId: number = 0;
+      
+        for (let message of this.messages) {
+          let currentId = +message.id;
+          if (currentId > maxId) {
+            maxId = currentId;
+          }
+        
+        }
+        return maxId;
+      }
 }
